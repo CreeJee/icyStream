27c41e59e64a6f1f16204ea04f5217244a6208e6
CreeJee <000as000@naver.com>
2017-06-08 01:59:42 +0900

    init commit
    thanks for raste

---
+++ index.js
@@ -0,0 +1,33 @@
+const icy = require('icy'),
+    express = require('express'),
+    app = express(),
+    url = 'http://sc08.saycast.com:8028';
+app.get('/',function(req,res,next) {
+    res.send('main');
+});
+app.get('/live',function(req, res, next) {
+    res.send('<audio controls><source src="/live.mp3" type="audio/mpeg"></audio>')
+})
+app.get('/live.mp3',function(req,res,next) {
+    // connect to the remote stream 
+    icy.get(url, function (icy_result) {
+        var headers = {
+            'content-type': 'audio/mpeg',
+            'Transfer-Encoding': 'chunked',
+        };
+        res.writeHead(200, headers);
+        icy_result.on('error',function(exception) {
+            console.error("Error reading file: ", exception);
+        })
+        icy_result.on('data',function(r) {
+            res.write(r);
+        });
+        icy_result.on('end',function() {
+            res.end();
+        })
+    });    
+});
+
+app.listen(process.env.PORT,process.env.IP,function () {
+  console.log(`Example app listening on port ${process.env.PORT}!`);
+});
